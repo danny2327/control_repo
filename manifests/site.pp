d4d9fd973f9695ab85a77204a79269b071889463
danny2327 <dstrood@gmail.com>
2020-05-07 13:09:55 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
  node default {
-   file {'/root/README':
-     ensure => file,
-     content => 'This is your readme file',
-     owner => 'root',
-     }
-    file {'/root/README':
-      owner => 'root',
-    }
  }
